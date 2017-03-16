bc2566077b00e90c267464da3cbb22afec4b4f0e
chandler <1941719093@qq.com>
2017-03-16 16:49:28 +0800

    finish spring cloud !

--- order/src/main/java/com/ysh/order/WebInit.java
+++ order/src/main/java/com/ysh/order/WebInit.java
@@ -3,9 +3,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
+import com.ysh.catalog.api.controller.CustomerController;
 
+@EnableEurekaClient
 @SpringBootApplication
+@EnableFeignClients(clients = CustomerController.class)
 public class WebInit {
 	
 	private static Logger logger = LoggerFactory.getLogger(WebInit.class);
